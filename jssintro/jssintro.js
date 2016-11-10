9bc4bc8d8f409e8f188f0930ea4167bd506ecec9
Bailey Storms <storb279@newschool.edu>
2016-11-10 19:53:16 +0000

    testing

---
+++ jssintro/jssintro.js
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>intro to js</title>
+        <link href="styes.css" rel="stylesheet" type="text/css"> 
+    </head>
+    <body>
+    </body>
+</html> 
---
+++ jswork/main.js
@@ -0,0 +1,33 @@
+console.log("Hello world");
+
+$("body").css("background-color","blue");
+
+var hasClicked = false;
+
+$("button").click(function() {
+    console.log(hasClicked);
+    $("body").css("background-color","red");
+    hasClicked = true;
+});
+
+for (var i = 0; i < 100; i++) {
+    $("body").append("<div class='go'></div>");
+}
+
+var counter = 0;
+
+$(".go").each(function() {
+    console.log(counter);
+    console.log("found a div with class go...");
+    $(".go").css("border-radius", counter + "20%");
+    counter++;
+})
+
+$(".go").click(function() {
+    console.log("you clicked it...");
+    $(this).css("background-color","lime");
+    $(this).html("Hello");
+});
+
+
+
